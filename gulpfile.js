918a89d28d0ef047c6717827d0377a067e1660dc
markovo4 <obf2007@gmail.com>
2024-04-09 17:22:07 +0300

    layout-project setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const {
-  parallel, src, dest, watch,
+  parallel, src, dest, watch, series,
 } = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
@@ -56,12 +56,20 @@ const imagesOptimize = (done) => {
 
   done();
 };
+// Run on <<gulp>> in GitBash => will run in parallel all 4 tasks
+exports.default = parallel(sassCompile, pugCompile, imagesOptimize, firstTask);
 
-const copyFile = () => src('dist/scss/index.scss')
-  .pipe(dest('build/styles'));
+// Run on <<gulp seriesDefault>> in GitBash => will run all 4 tasks one after another
+exports.seriesDefault = series(sassCompile, pugCompile, imagesOptimize, firstTask);
 
 const copyScss = () => src(['dist/**/*.scss', '!dist/project/**'])
   .pipe(dest('build/styles'));
+
+// Run on <<gulp copyScssFiles>> in GitBash => will search in dist/ in all <<**> directories for any <<*>> files
+// with extension <<.scss>> and exclude <<!>> from search any <<**>> directories/files within <</project/>> directory.
+// After the search <<src()>> using Globs <<['dist/**/*.scss', '!dist/project/**']>> files will be copied and placed
+// using <<dest()>> into <<'build/styles'>>.
+exports.copyScssFiles = copyScss;
 
 //
 const changeAppStylesFile = (done) => {
@@ -85,14 +93,6 @@ const watchers = () => {
 };
 //
 
-exports.default = parallel(sassCompile, pugCompile, imagesOptimize);
-exports.layoutCompile = parallel(sassCompile, pugCompile);
-exports.assetsOptimize = imagesOptimize;
-
-exports.default = firstTask;
-
-exports.copy = copyFile;
-exports.copy = copyScss;
 exports.watchers = watchers;
 exports.build = parallel(buildSass, buildPug);
 exports.server = browserSyncJob;
